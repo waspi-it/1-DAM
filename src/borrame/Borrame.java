3a395923ccbc701472bb612bd00c902c9ad75505
dam <dam@203P11>
2026-03-16 11:44:22 +0100

    rama principal

---
+++ src/borrame/Borrame.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package borrame;
+
+/**
+ *
+ * @author dam
+ */
+public class Borrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hoy es lunes");
+    }
+    
+}
